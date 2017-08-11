95987cdc559263ce15089ac9fe16f0304d381d02
Audiodrome <lien.daniel.h@gmail.com>
2017-08-11 12:15:41 -0700

    initial commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+
+  var elementList = []; // Contains element node with className of document.body
+
+  var findClassName = function(element) {
+    if (element.classList && element.classList.contains(className)) {
+        // saves an element node that contains the className;
+        elementList.push(element);
+    }
+    for (var i = 0; i < element.childNodes.length; i++) {
+      // exhaustive search of all element childNodes looking for className;
+      findClassName(element.childNodes[i]);
+    }
+  }
+
+  findClassName(document.body);
+  // console.log(elementList);
+  return elementList;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,68 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  // // Primitives ***************************************************************
+  // if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
+  //   return '' + obj;
+  // } else if (typeof obj === 'string') {
+  //   return '"' + obj + '"';
+  //
+  // // Arrays [] ****************************************************************
+  // } else if (Array.isArray(obj)) {
+  //   var storage = [];
+  //   if (obj[0] === undefined) {
+  //     return '[]';
+  //   } else {
+  //     for (var i = 0; i < obj.length; i++) {
+  //       storage.push(stringifyJSON(obj[i]));
+  //     }
+  //     //console.log(storage);
+  //     return '[' + storage + ']';
+  //   }
+  // }
+  // Object Literals {} *******************************************************
+
+  if (obj === null) { return 'null'; }
+
+  if (obj.constructor === String) { return '"' + obj + '"'; }
+
+  if (obj.constructor === Array) {
+    if (obj.length > 0) {
+      var storage = [];
+      for (var i = 0; i < obj.length; i++) {
+        storage.push(stringifyJSON(obj[i]));
+      }
+      return '[' + storage + ']';
+    } else {
+      return '[]';
+    }
+  }
+
+  if (obj.constructor === Object) {
+    // Get all the keys
+    var keys = Object.keys(obj);
+
+    if (keys.length > 0) {
+      var storage = '';
+
+      for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        // Conditional: Key exists and is neither a function nor points to one.
+        if (key && obj[key] !== undefined && typeof key !== 'function' && typeof obj[key] !== 'function') {
+          // Conditional: Is this the last key?
+          if (i === keys.length - 1) {
+            storage += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+          } else {
+            storage += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+          }
+        }
+      }
+      return '{' + storage + '}';
+    } else {
+      return '{}';
+    }
+  }
+
+  return obj.toString();
 };
